f7a8111449b6a296cf91705f0356887f1896cf69
matheusbtguerra <matheus05guerra@gmail.com>
2024-11-24 17:49:07 -0300

    feat: creates a validator template for user request

---
+++ user/src/main/java/com/caldeira/projetofinal/validators/UserRequestValidator.java
@@ -0,0 +1,15 @@
+package com.caldeira.projetofinal.validators;
+
+import com.caldeira.projetofinal.user.models.request.UserRequestModel;
+import org.springframework.stereotype.Component;
+@Component
+public class UserRequestValidator {
+    private void validate(UserRequestModel model){
+        if (model.getFirstName() == null || model.getFirstName().length() < 3) {
+            throw new IllegalArgumentException("'firstName' cannot be null or less than 3 characters long.");
+        }
+        if (model.getLastName() == null || model.getLastName().length() < 3) {
+            throw new IllegalArgumentException("'lastName' cannot be null or less than 3 characters long.");
+        }
+    }
+}
