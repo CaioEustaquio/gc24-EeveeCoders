cc293f8d61c0f53fe09a36e1d0f612933c7942b4
matheusbtguerra <matheus05guerra@gmail.com>
2024-11-26 20:49:08 -0300

    feat: creates model for game response

---
+++ zelda/src/main/java/com/caldeira/projetofinal/zelda/models/GameResponseModel.java
@@ -0,0 +1,20 @@
+package com.caldeira.projetofinal.zelda.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GameResponseModel {
+    private String name;
+    private String description;
+    private String developer;
+    private String publisher;
+    private LocalDateTime released_date;
+    private UUID id;
+}
