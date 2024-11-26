5ebe7bfd2a3f979d61a0da7bdf2afc3e7f5a7832
alunojava-21 <alunojava-21@MacBook-Pro-de-Administrador-2.local>
2024-11-26 15:14:32 -0300

    fix: fix entity structure and add lombok annotations

--- user/src/main/java/com/caldeira/projetofinal/user/entities/UserEntity.java
+++ user/src/main/java/com/caldeira/projetofinal/user/entities/UserEntity.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -14,6 +16,8 @@ import java.util.UUID;
 @Getter
 @Setter
 @Table (name = "user_entity")
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
     @Id
     @GeneratedValue
@@ -21,15 +25,4 @@ public class UserEntity {
     private String firstName;
     private String lastName;
     private LocalDateTime creationDate;
-
-    //construtor explicito padrao
-    public UserEntity(UUID id, String firstName, String lastName, LocalDateTime creationDate) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.creationDate = creationDate;
-    }
-
-    //construtor explicito, JPA precisa para instanciar
-    public UserEntity(){}
 }
