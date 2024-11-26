d3f80da94b8dfb178634164902c06db452b17a8f
Erika-eni <erikaafedrigo@gmail.com>
2024-11-25 21:27:47 -0300

    fiz feat: Integer to UUID

--- user/src/main/java/com/caldeira/projetofinal/user/repositories/UserRepository.java
+++ user/src/main/java/com/caldeira/projetofinal/user/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import com.caldeira.projetofinal.user.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity,Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 }
