d4cc70dc17ea59b560b95aca1d16fbc0bf9c4558
alunojava-21 <alunojava-21@MacBook-Pro-de-Administrador-2.local>
2024-11-26 16:02:36 -0300

    fix: fix entity structure and add lombok annotations

--- user/src/main/java/com/caldeira/projetofinal/user/entities/UserEntity.java
+++ user/src/main/java/com/caldeira/projetofinal/user/entities/UserEntity.java
@@ -1,9 +1,6 @@
 package com.caldeira.projetofinal.user.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,9 +17,15 @@ import java.util.UUID;
 @NoArgsConstructor
 public class UserEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private UUID id;
+
+    @Column (name = "first_name")
     private String firstName;
+
+    @Column (name = "last_name")
     private String lastName;
+
+    @Column (name = "creation_date")
     private LocalDateTime creationDate;
 }
