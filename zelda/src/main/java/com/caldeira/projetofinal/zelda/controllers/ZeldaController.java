038005082692d04f623b9f815e476351ce4bb7d9
LisianeH <lisiane.h67@gmail.com>
2024-12-02 17:42:33 -0300

    feat: creates function get all games

--- zelda/src/main/java/com/caldeira/projetofinal/zelda/controllers/ZeldaController.java
+++ zelda/src/main/java/com/caldeira/projetofinal/zelda/controllers/ZeldaController.java
@@ -1,17 +1,26 @@
 package com.caldeira.projetofinal.zelda.controllers;
 
+import com.caldeira.projetofinal.zelda.models.GameModel;
 import com.caldeira.projetofinal.zelda.services.ZeldaGatewayService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/zelda")
 public class ZeldaController {
 
     @Autowired
-    private ZeldaGatewayService restTemplate;
+    private ZeldaGatewayService zeldaGatewayService;
 
-
-
+    @GetMapping("/get-all")
+    public ResponseEntity<List<GameModel>> getAll(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer size
+    ) {
+        List<GameModel> games = zeldaGatewayService.getAllGames(page, size);
+        return ResponseEntity.ok(games);
+    }
 }
