a64402c15e338ba3e79ea31b3735337256316368
Pablo Gascón <pablogasconiel445@gmail.com>
2024-12-03 17:25:48 -0300

    test: adding mockito dependecy and base test class

---
+++ zelda/src/test/java/com/caldeira/projetofinal/zelda/services/ZeldaGatewayServiceTest.java
@@ -0,0 +1,10 @@
+package com.caldeira.projetofinal.zelda.services;
+
+import org.mockito.Mock;
+
+public class ZeldaGatewayServiceTest {
+    @Mock
+    private ZeldaGatewayService zeldaGatewayService;
+
+
+}
