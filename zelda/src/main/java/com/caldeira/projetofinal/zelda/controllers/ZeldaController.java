f641c4622a9adfed9d032a5efbf99aff29245681
LisianeH <lisiane.h67@gmail.com>
2024-12-03 09:36:57 -0300

    feat: created get by id

--- zelda/src/main/java/com/caldeira/projetofinal/zelda/controllers/ZeldaController.java
+++ zelda/src/main/java/com/caldeira/projetofinal/zelda/controllers/ZeldaController.java
@@ -1,5 +1,6 @@
 package com.caldeira.projetofinal.zelda.controllers;
 
+import com.caldeira.projetofinal.zelda.models.GameListResponseModel;
 import com.caldeira.projetofinal.zelda.models.GameModel;
 import com.caldeira.projetofinal.zelda.services.ZeldaGatewayService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,22 @@ public class ZeldaController {
     private ZeldaGatewayService zeldaGatewayService;
 
     @GetMapping("/get-all")
-    public ResponseEntity<List<GameModel>> getAll(
+    public ResponseEntity<GameListResponseModel> getAll(
             @RequestParam(required = false) Integer page,
             @RequestParam(required = false) Integer size
     ) {
         List<GameModel> games = zeldaGatewayService.getAll(page, size);
-        return ResponseEntity.ok(games);
+        GameListResponseModel response = new GameListResponseModel(true, games.size(),  games);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get-by-id/{id}")
+    public ResponseEntity<GameModel> getById(@PathVariable("id") String id) {
+        GameModel gameModel = zeldaGatewayService.getById(id);
+        if (gameModel != null) {
+            return ResponseEntity.ok(gameModel);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
